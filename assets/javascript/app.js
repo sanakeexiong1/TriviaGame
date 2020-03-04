34edf40f28d50e8679e91e9c9e23f22e5ad96f3d
VinylBubbles <xiongsanakee@gmail.com>
2020-03-03 20:29:55 -0700

    Started work on JS, completed objects for answer/questions, completed pseudocoding the rest of the problem

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+
+//Create objects for the questions
+var questions = {
+    question1:"What fruit is stinky and has lots of spikes?",
+    question2:"What fruit is green on the outside, sweet, and has either red or white flesh?",
+    question3:"What fruit has soft sweet flesh and many seeds with a purple skin?",
+    question4:"What sweet purple skin fruit has white flesh and a seed in each sliver?",
+    question5:"What brown hairy fruit is known for it's 'water' and delicious white meat?",
+    question6:"What flat orange fruit is this?",
+    question7:"What purple or yellow skinned fruit is known for it's sweet and sour taste with lots of seeds?",
+    question8:"What fruit is shown here? (Hint:You can eat it ripe or raw)",
+    question9:"What dried fruit is sweet and similar to a prune?",
+    question10:"What fruit has many seeds and is neutral in flavor while looking like a mythical creature's scale?",
+
+}
+
+//Create objects for the answers
+var answers = {
+    answer1:"Durian",
+    answer2:"Guava",
+    answer3:"Fig",
+    answer4:"Mangosteen",
+    answer5:"Coconut",
+    answer6:"Persimmon",
+    answer7:"Passionfruit",
+    answer8:"Papaya",
+    answer9:"Date",
+    answer10:"Dragon Fruit",
+}
+
+//Create array for answer choices for each question
+//Create buttons for each answer choice in the array
+//Append img and buttons for each question to the HTML
+//Create for loop for if/else statements regarding user choice
+    //If userchoice matches the answer then "you win", else "you lose".
+    //If "You lose" move to next question.
+//Set time settings of 1 minute total to complete game
+//Reset and move onto next question
