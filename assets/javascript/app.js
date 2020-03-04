41d8095f4000cbc0d5d966c4e3ec3ac2ebbf6840
VinylBubbles <xiongsanakee@gmail.com>
2020-03-03 20:54:14 -0700

    started to create buttons for each item in the array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,23 +16,40 @@ var questions = {
 
 //Create objects for the answers
 var answers = {
-    answer1:"Durian",
-    answer2:"Guava",
-    answer3:"Fig",
-    answer4:"Mangosteen",
-    answer5:"Coconut",
-    answer6:"Persimmon",
-    answer7:"Passionfruit",
-    answer8:"Papaya",
-    answer9:"Date",
-    answer10:"Dragon Fruit",
+    answer1:"a: Durian",
+    answer2:"b: Guava",
+    answer3:"a: Fig",
+    answer4:"d: Mangosteen",
+    answer5:"b: Coconut",
+    answer6:"c: Persimmon",
+    answer7:"b: Passionfruit",
+    answer8:"d: Papaya",
+    answer9:"a: Date",
+    answer10:"d: Dragon Fruit",
 }
 
 //Create array for answer choices for each question
+var question1choices = ["a: Durian", "b: Guava", "c: Plum", "d: Orange"];
+var question2choices = ["a: Clementine", "b: Guava", "c: Passionfruit", "d: Peach"];
+var question3choices = ["a: Fig", "b: Orange", "c: Plum", "d: Banana"];
+var question4choices = ["a: Durian", "b: Guava", "c: Plum", "d: Mangosteen"];
+var question5choices = ["a: Banana", "b: Coconut", "c: Plum", "d: Peach"];
+var question6choices = ["a: Orange", "b: Guava", "c: Persimmon", "d: Mangosteen"];
+var question7choices = ["a: Durian", "b: Passionfruit", "c: Plum", "d: Dragonfruit"];
+var question8choices = ["a: Durian", "b: Guava", "c: Mangosteen", "d: Papaya"];
+var question9choices = ["a: Date", "b: Guava", "c: Plum", "d: Papaya"];
+var question10choices = ["a: Fig", "b: Apple", "c: Dragonfruit", "d: Orange"];
 //Create buttons for each answer choice in the array
-//Append img and buttons for each question to the HTML
+function createButtons1() {
+    for (var i=0; i < question1choices.length; i++) {
+        document.getElementById("choices").innerHTML = "<button>" + question1choices[i] + "</button>";
+    }
+}
+
+createButtons1();
 //Create for loop for if/else statements regarding user choice
     //If userchoice matches the answer then "you win", else "you lose".
     //If "You lose" move to next question.
 //Set time settings of 1 minute total to complete game
+
 //Reset and move onto next question
